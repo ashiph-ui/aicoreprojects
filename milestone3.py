c8292b9cbfd49c43b3422e6f87ec94ffd651db3c
ashiph-ui <arai@DESKTOP-Q3IFECL.>
2024-11-29 15:17:43 +0000

    adds task 3

--- milestone3.py
+++ milestone3.py
@@ -1,8 +1,18 @@
+import random
+
+word_list = ["apple", "banana", "orange", "grapes","strawberry"]
+word = random.choice(word_list)
+print(word)
+
 while True:
-    guess = input("Guess a letter")
+    guess = input("Guess a letter: ")
     if guess.isalpha and len(guess) == 1:
         print("Valid guess")
-        continue
+        
     else:
-        guess = input("Invalid letter. Please, enter a single alphabetical character.")
-    
+        guess = input("Invalid letter. Please, enter a single alphabetical character: ")
+        
+    if guess in word:
+            print(f"Good guess! {guess} is in the word.")   
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again")  
