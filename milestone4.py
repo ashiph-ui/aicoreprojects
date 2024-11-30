2d926bc8b92d9687f590a4e3dde9597e81b9081c
ashiph-ui <arai@DESKTOP-Q3IFECL.>
2024-11-30 15:29:53 +0000

    code for task 2

--- milestone4.py
+++ milestone4.py
@@ -14,7 +14,30 @@ class Hangman():
         self.num_letters = len(set([i for i in self.word]))
         self.list_of_guesses = []
         
-    def test(self):
+    def check_guess(self, guess):
+        guess = guess.lower()
+    
+        if guess in self.word:
+                    print(f"Good guess! {guess} is in the word.")   
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again")
+            
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter: ").lower()
+      
+            if not guess.isalpha and len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            
+            elif guess in self.word_guessed:
+                print("You already tried that letter!")
+            
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                    
+            
+    def check_attributes(self):
         print(self.word)
         print(self.word_guessed)
         print(self.num_letters)
@@ -23,7 +46,7 @@ class Hangman():
 word_list = ["apple", "banana", "orange", "grapes","strawberry"]
 test = Hangman(word_list)
 
-test.test()
+test.ask_for_input()
 
         
         
